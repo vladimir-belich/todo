4d6644444788e92087aabb97d4693ae27d7d219c
Vladimir Belich <vladimir.belich@gmail.com>
2020-11-06 23:59:13 +0200

    add bootstrap

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
+gem 'bootstrap', '~> 4.4', '>= 4.4.1'
 gem 'dotenv-rails', '~> 2.2'
 gem 'pg', '~> 1.2', '>= 1.2.3'
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,11 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+require("bootstrap")
+require("jquery")
+
+import JQuery from 'jquery';
+window.$ = window.JQuery = JQuery;
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
